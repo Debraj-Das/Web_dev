2db00d1f44bd69187746446502f201316438ad7c
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-10-29 17:36:51 +0530

    Start working on web development

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //* Basic of javascript
-// const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 // let myname = prompt("Enter your name : ");
 
@@ -15,7 +15,6 @@
 
 // console.log(myname);
 
-
 // Practise the Code the function
 
 // function sum(a, b)
@@ -27,4 +26,30 @@
 
 // Some practise Question
 
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft\n");
+  let numberOfBottles = Math.floor(money / 1.5);
+  console.log("buy " + numberOfBottles + " bottles of milk");
+  console.log("\nmoveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+  return money % 1.5;
+}
 
+let money = prompt("Enter the money : ");
+let change = getMilk(money);
+console.log("\nYour change is " + change);
